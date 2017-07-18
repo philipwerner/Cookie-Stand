84824f079839eee65137a86f56d8b77d92cf3831
philipwerner <philip.r.werner@gmail.com>
2017-07-17 18:22:54 -0700

    push to array not working, js template set need to finish messages

--- app.js
+++ app.js
@@ -1,29 +1,106 @@
 'use strict'
-var custPerHour = function(min, max) {
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-}
-custPerHour.prototype.generateRandom = function(min, max) {
-  return Math.floor(Math.random()*(max - min + 1)) + min;
-}
-var times = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
+
+// Random customer function
+var times = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var stores = ['1st and Pike', 'SeaTac', 'Seattle Center', 'Capital Hill', 'Alki'];
 // 1st and Pike
 var pike = {
+  name: '1st and Pike',
   minCust: 23,
   maxCust: 65,
   avgCookie: 6.3,
-  custEachHour: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
-  cookiesEachHour: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  totalSales: 0,
+  custEachHour: [],
   totalCookie: 0,
-  render: function(){
-    var pikeUL = document.getElementById('pike');
-      for(i = 0; i < this.cookiesEachHour.length; i++){
-        var liEL = document.createElement('li');
-        liEL.textContent = times[i] + ': ' + this.cookiesEachHour[i];
-        pikeUL.appendChild(liEL)
-      }
-  }
-  }
-}
+  custPerHour:
+    function(maxCust, minCust) {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust) + 1) + this.minCust
+    }
+};
 
-// Random customer function
+var seaTac = {
+  name: 'SeaTac',
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+  totalSales: 0,
+  custEachHour: [],
+  totalCookie: 0,
+  custPerHour:
+    function(maxCust, minCust) {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust) + 1) + this.minCust
+    }
+};
+
+var seaCenter = {
+  name: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+  totalSales: 0,
+  custEachHour: [],
+  totalCookie: 0,
+  custPerHour:
+    function(maxCust, minCust) {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust) + 1) + this.minCust
+    }
+};
+
+var capHill = {
+  name: 'Capital Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3,
+  totalSales: 0,
+  custEachHour: [],
+  totalCookie: 0,
+  custPerHour:
+    function(maxCust, minCust) {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust) + 1) + this.minCust
+    }
+};
+
+var alki = {
+  name: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6,
+  totalSales: 0,
+  custEachHour: [],
+  totalCookie: 0,
+  custPerHour:
+    function(maxCust, minCust) {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust) + 1) + this.minCust
+    }
+};
+
+  // function custPerHour() {
+  //   for (var i = 0; i < this.custEachHour.length; i++) {
+  //     this.custEachHour[i] = randomCust(this.minCust, this.maxCust);
+  //   }
+  // },
+  // function cookiesSold(){
+  //   for (var i = 0; i < this.cookiesEachHour.length; i++);
+  //   this.cookiesEachHour[i] = this.custEachHour[i] * this.avgCookie;
+  // },
+  // render: function(){
+  //   var pikeUL = document.getElementById('pike');
+  //   for (i = 0; i < this.cookiesEachHour.length; i++){
+  //   var liEL = document.createElement('li');
+  //   liEL.textContent = times[i] + ': ' + this.cookiesEachHour[i];
+  //   pikeUL.appendChild(liEL)
+  //   }
+  // }
+console.log(pike.custEachHour)
+console.log(pike.custPerHour())
+console.log(pike.hourlyCust());
+// pike.render();
+
+// hourlyCust:
+//   function() {
+//     for(var i = 0; i < times.length; i++){
+//       this.custEachHour.push(this.custPerHour())
+//       this.totalSales += this.custEachHour[i]
+//     }
+//     return
+//   }
