7b9147d8a6abb1b0acf191af51259fa666e6166f
philipwerner <philip.r.werner@gmail.com>
2017-07-20 22:37:58 -0700

    continued working on js for order form, having issue getting the order total math

--- order.js
+++ order.js
@@ -10,12 +10,30 @@ var Order = function(firstName, lastName, address, city, state, zipcode, quantit
   this.state = state;
   this.zipcode = zipcode;
   this.quantity = quantity;
+  this.order = [];
+  this.calcOrderTotal = function(){
+    for(var i = 0; i < myOrders.length; i++){
+      this.order.push(Math.ceil(this.quantity[i] * 2));
+      this.orderTotal += this.order[i];
+    }
+  };
+  this.orderTotal = 0;
+  myOrders.push(this);
 };
+
+Order.prototype.render = function(){
+  for(var i = 0; i < myOrders.length; i++){
+    alert('Your total is $' + this.order[i]);
+  }
+};
+
+// function yourTotal(){
+// };
 
 function handleOrderSubmit(event) {
   event.preventDefault();
 
-  if(!event.target.firstName.value || !event.target.lastName.value || !event.target.address.value || !event.target.city.value || event.target.state.value || event.target.zipcode.value || event.target.quantity.value) {
+  if(!event.target.firstName.value || !event.target.lastName.value || !event.target.address.value || !event.target.city.value || !event.target.state.value || !event.target.zipCode.value || !event.target.qty.value) {
     return alert('All fields must be filled out to place an order');
   }
 
@@ -24,8 +42,8 @@ function handleOrderSubmit(event) {
   var address = event.target.address.value;
   var city = event.target.city.value;
   var state = event.target.state.value;
-  var zipcode = event.target.zipcode.value;
-  var quantity = event.target.quantity.value;
+  var zipcode = event.target.zipCode.value;
+  var quantity = parseInt(event.target.qty.value);
 
   var newOrder = new Order(firstName, lastName, address, city, state, zipcode, quantity);
 
@@ -34,11 +52,12 @@ function handleOrderSubmit(event) {
   event.target.address.value = null;
   event.target.city.value = null;
   event.target.state.value = null;
-  event.target.zipcode.value = null;
-  event.target.quantity.value = null;
+  event.target.zipCode.value = null;
+  event.target.qty.value = null;
 
   myOrders.push(newOrder);
   newOrder.render();
+  // yourTotal();
 };
 
 newOrderForm.addEventListener('submit', handleOrderSubmit);
