f43aaa5d65c944aa50329db970c2520aab3407e2
Philip Werner <philip.r.werner@gmail.com>
2018-02-13 10:53:38 -0800

    fixed more linter errors.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // Random customer function
 var times = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-var stores = ['1st and Pike', 'SeaTac', 'Seattle Center', 'Capital Hill', 'Alki'];
+// var stores = ['1st and Pike', 'SeaTac', 'Seattle Center', 'Capital Hill', 'Alki'];
 
 // 1st and Pike
 var pikePlace = {
